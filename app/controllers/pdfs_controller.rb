37952221ffc1cbf7ee4c8c2d573720c4045b71cf
Tom Taylor <tommotaylor@gmail.com>
2019-10-08 19:14:49 +0100

    WIP

--- app/controllers/pdfs_controller.rb
+++ app/controllers/pdfs_controller.rb
@@ -1,7 +1,8 @@
 class PdfsController < ActionController::Base
   def create
-    html = render_to_string(action: "show", layout: 'application')
+    html = render_to_string(action: "show", layout: false)
     kit = PDFKit.new(html)
+    binding.pry
     pdf = kit.to_pdf
 
     send_data pdf, type: 'application/pdf',
