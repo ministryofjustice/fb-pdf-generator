1de170791170a78a908c6529e0344862824f0457
cob16 <public+github@cormacbrady.info>
2019-10-08 14:41:00 +0100

    add auth usecase and gateway

---
+++ app/lib/gateway/authentication.rb
@@ -0,0 +1,42 @@
+require 'httparty'
+module Gateway
+  class Authentication
+    class AuthenticationApiError < StandardError
+      attr_reader :response
+      def initialize(response)
+        super
+        @response = response
+      end
+
+      def to_s
+        uri = response&.request&.last_uri.to_s
+        "[Authentication Api Error: Received #{response&.code} response from #{uri}] #{super}"
+      end
+    end
+
+    include HTTParty
+    format :json
+
+    def initialize(token_api_base_url:, service_slug:)
+      self.class.base_uri token_api_base_url
+
+      @service_slug = service_slug
+    end
+
+    def hmac_secret
+      Rails.logger.error('this is a stub auth!')
+      request_hmac_secret
+    end
+
+    private
+
+    attr_reader :service_slug
+
+    def request_hmac_secret
+      response = self.class.get("/service/#{service_slug}")
+      raise AuthenticationApiError, response unless response.success?
+
+      response.fetch('token')
+    end
+  end
+end
