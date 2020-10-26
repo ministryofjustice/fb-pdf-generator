9969944623205887a4e8609a79e5d0bb3cd67bc4
brenetic <brendan.butler@digital.justice.gov.uk>
2020-10-26 18:36:08 +0000

    Change controller to read JSON requests
    Previously the controller was using www-form-urlencoded and Rails was
    unable to parse the params correctly. This was failing specifically when
    users entered a "%" character into their submissions.
    
    Swap to use the params itself and symbolize all the param keys.
    
    Co-authored-by: Tomas Destefi <tomas.destefi@digital.justice.gov.uk>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope 'v1' do
-    resources :pdfs, only: [:create]
+    resources :pdfs, only: [:create], format: :json
   end
 
   get '/health', to: 'health#show'
