f3958d7e140f26bf8d115552038d79a3826ee7cb
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-05-31 11:55:47 +0100

    WIP

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails'
-require 'action_controller/railtie'
-require 'action_view/railtie'
-require 'pdfkit'
+require "rails"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "pdfkit"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,19 +14,19 @@ module FbPdfGenerator
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Disable request forgery protection as this in an api
-    config.action_controller.allow_forgery_protection = false
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
+    config.action_controller.allow_forgery_protection = false
     config.middleware.use PDFKit::Middleware
   end
 end
